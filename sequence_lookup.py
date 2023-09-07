69714fe1c10ffb17b52094922ef5cff6ed5acee3
rowanwoodell <rowanwoodell@gmail.com>
2023-09-07 18:03:11 +0100

    Cleanup, refactors, comments

--- sequence_lookup.py
+++ sequence_lookup.py
@@ -1,31 +1,42 @@
-import contfrac as cf
-import math
 import itertools
-from scipy.optimize import root_scalar
 from mpmath import mp
 import time
 
-mp.dps = 52
-
 
 class SequenceLookup:
-    def __init__(self, parent, power):
-        self.parent = parent
-        self.power = power
+    '''
+    Class to store information about sequences in order to find all weights 
+    without computing them all.
+    '''
+    def __init__(self, atom, freq):
+        self.atom = atom
+        self.freq = freq
         self.weight = None
 
-    def setParent(self, p):
-        self.parent = p
+    def setAtom(self, a):
+        self.atom = a
 
     def setWeight(self, w):
         self.weight = w
 
+    def isAtomic(self):
+        if self.freq == 1:
+            return True
+        else:
+            return False
+
 
 def rotate_list(lst, n):
+    '''
+    Returns a list with the same entries as lst, cycled n places to the left.
+    '''
     return lst[n:] + lst[:n]
 
 
 def find_all_sequences(family, n):
+    '''
+    Returns all sequences of length n with entries in the list family.
+    '''
     if n <= 0:
         return ()
     if n == 1:
@@ -42,105 +53,41 @@ def find_all_sequences(family, n):
 
 
 def weight(s, acc):
-    # seq = []
-    # for x in s:
-    #     seq.append(int(x))
+    '''
+    Returns the weight of the sequence s.
+    '''
     seq = list(s)
     w = mp.mpf(1)
+
+    # Cycle seq, find the value of the purely periodic cf whose coefficients 
+    # are the cycled seq, and multiply w by this value. Repeat for each cycle.
     for i in range(len(seq)):
-        # w *= cf.evaluate([0] + (rotate_list(seq, i) * acc))
-        # w *= my_cf_eval(rotate_list(seq, i) * acc)
-        w = mp.fmul(w, precise_cf_evaluate([0] + rotate_list(seq, i) * acc))
-    # mp.nprint(w)
+        w = mp.fmul(w, precise_cf_evaluate(rotate_list(seq, i) * acc))
     return w
 
 
 def precise_cf_evaluate(cont_frac):
-    """Computes the floating point value of a finite continued fraction
-    representation.
-
-    That is the value of ``c[0] + 1/(c[1] + 1/(c[2] + 1/(c[3] + ...)))``
-    for an input ``c``.
-
-    Example:
-        For an input of ``[2,3,4]`` is ``2 + 1/(3 + 1/4) = 30/13`` expressed as
-        2.3076923076923075.
-
-    Args:
-        cont_frac (Iterable[Union[int, float]]): representation of a continued
-            fraction as iterable of numbers.
-
-    Returns:
-        float: the value of the continued fraction.
-    """
+    '''
+    Returns the real number with continued fraction coefficients given by the 
+    list cont_frac. Adapted from the cf_evalute function in the contfrac 
+    library in order to support mpmath operations. Also assumes the leading
+    coefficient is a zero and has been omitted, i.e. the value is between 0 
+    and 1 (as is always the case for us).
+    '''
     if len(cont_frac) == 0:
         return 0
+    
     fraction = 0
-    for i, coefficient in enumerate(reversed(cont_frac[1:])):
+    for coefficient in reversed(cont_frac):
         fraction = mp.fdiv(1, (mp.fadd(coefficient, fraction)))
-    return mp.fadd(cont_frac[0], fraction)
+    return fraction
 
-
-def my_cf_eval(cont_frac):
-    p_continuants = [0, 1, 0]
-    q_continuants = [1, 0, 1]
-    n = len(cont_frac)
-
-    p_n = p(n, cont_frac, p_continuants)
-    p_n_minus_one = p_continuants[n + 1]
-
-    q_n = q(n, cont_frac, q_continuants)
-    q_n_minus_one = q_continuants[n + 1]
-
-
-
-    # print(p_n, p_n_minus_one)
-    # print(q_n, q_n_minus_one)
-
-    # print(p_n - q_n_minus_one)
-    # print(math.sqrt(math.pow(q_n_minus_one - p_n, 2) + 4 * q_n * p_n_minus_one))
-    # print(2 * q_n)
-
-    numerator = p_n - q_n_minus_one + math.sqrt(math.pow(q_n_minus_one - p_n, 2) + 4 * q_n * p_n_minus_one)
-    denominator = 2 * q_n
-
-    return(numerator / denominator)
-
-
-def p(n, cont_frac, p_continuants):#, continuants=[0, 1, 0]):
-    m = n + 2
-
-    if m <= len(p_continuants) - 1:
-        return p_continuants[m]
-    
-    a_n = cont_frac[n - 1]
-    p_n = a_n * p(n - 1, cont_frac, p_continuants) + p(n - 2, cont_frac, p_continuants)
-
-    p_continuants.append(p_n)
-
-    return p_n
-
-
-def q(n, cont_frac, q_continuants):
-    m = n + 2
-
-    if m <= len(q_continuants) - 1:
-        return q_continuants[m]
-    
-    a_n = cont_frac[n - 1]
-    q_n = a_n * q(n - 1, cont_frac, q_continuants) + q(n - 2, cont_frac, q_continuants)
-
-    q_continuants.append(q_n)
-
-    return q_n
-
-# for i in range(5):
-#     print(find_all_sequences(alph, i))
-
-# for i in range(4):
-#     print(rotate_list(find_all_sequences(alph, 4)[1], i))
 
 def get_cyclic_perm_classes(seq, n):
+    '''
+    Returns the cyclic permutation classes for the sequences of length n in the
+    list seq.
+    '''
     cyclic_perm_classes = []
 
     for s in seq:
@@ -164,54 +111,80 @@ def get_cyclic_perm_classes(seq, n):
     return cyclic_perm_classes
 
 
-def initialise_lookup(alph, n):
+def initialise_lookup(family, n):
+    '''
+    Returns a list of SequenceLookup objects, one for each sequence of length 
+    n. Each SequenceLookup has its atom and freq fields correctly assigned,
+    but no weight assigned, as this is handled by the function get_full_lookup.
+    '''
+    seq = find_all_sequences(family, n)
 
-    seq = find_all_sequences(alph, n)
-
-    seq_parents = []
+    # For each sequence of length n, find its atom and create a SequenceLookup
+    # object with the atom and the frequency of the sequence.
+    # A potential speedup here would be to break when i > n/2, since the 
+    # frequency must divide n.
+    seq_atoms = []
     for s in seq:
         for i in range(1, n + 1):
             r = rotate_list(s, i)
+            # We cycle the sequence until the cycled version is equal to the
+            # uncycled version; the sequence is atomic iff we cycle all the way
+            # through the sequence, else we know the atomic part and create a
+            # SequenceLookup object with the relevant information.
             if s == r:
-                # print(s, r)
-                seq_parents.append(SequenceLookup(s[:i], int(n / i)))
+                atom = s[:i]
+                freq = n // i
+                seq_atoms.append(SequenceLookup(atom, freq))
                 break
+    
+    # Create a dictionary which assigns SequenceLookup objects to their
+    # original sequences.
+    seq_lookup = dict(zip(seq, seq_atoms))
 
-    seq_lookup = dict(zip(seq, seq_parents))
-
+    # Assign the same atom to all sequences in the same cyclic permutation 
+    # class.
     cyclic_perm_classes = get_cyclic_perm_classes(seq, n)
-
     for s in seq_lookup:
         for c in cyclic_perm_classes:
-                if s in c:
-                    slice = n // seq_lookup[s].power
-                    seq_lookup[s].setParent(c[0][:slice])
-                    break
+            if s in c:
+                period = n // seq_lookup[s].freq
+                seq_lookup[s].setAtom(c[0][:period])
+                break
 
     return seq_lookup
 
 
-def get_full_lookup(alph, N, acc):
-    lookup =  [initialise_lookup(alph, n) for n in range(N + 1)]
+def get_full_lookup(family, N, acc):
+    '''
+    Returns a list of SequenceLookup objects, one for each sequence of length 
+    m with entries in family, with 1 <= m <= n. Each SequenceLookup has all of
+    its fields correctly assigned.
+    '''
+    lookup =  [initialise_lookup(family, n) for n in range(N + 1)]
 
+    # Assign weights to elements of lookup by the following logic:
+    # if the sequence is atomic, calculate its weight from scratch;
+    # else, use its freq and the weight of its atom to find its weight. 
     for l in lookup:
         for s in l:
-            if l[s].power == 1:
-                w = weight(l[s].parent, acc)
+            if l[s].isAtomic():
+                w = weight(l[s].atom, acc)
             else:
-                par = l[s].parent
-                pow = l[s].power
-                n = len(par)
-                
-                w = mp.power(lookup[n][par].weight, pow)
+                atm = l[s].atom
+                frq = l[s].freq
+                n = len(atm)
+                w = mp.power(lookup[n][atm].weight, frq)
             
             l[s].setWeight(w)
     
     return lookup
 
 
-# From https://jeromekelleher.net/generating-integer-partitions.html
 def accel_asc(n):
+    '''
+    Returns a list containing all partitions of n.
+    Code from https://jeromekelleher.net/generating-integer-partitions.html
+    '''    
     a = [0 for i in range(n + 1)]
     k = 1
     y = n - 1
@@ -234,28 +207,33 @@ def accel_asc(n):
         yield a[:k + 1]
 
 
-# Returns a list containing k lists of lists of integers.
-# The n-th list consists of all partitions of k of length n.
-def r_tuples(k):
+def find_all_compositions(k):
+    '''
+    Returns a list containing k lists of lists of integers. The n-th list 
+    consists of all compositions of k of length n.
+    '''
     if k == 0:
         return None
     
+    # Find all partitions of k and sort them by length.
     partitions = list(accel_asc(k))
     partitions.sort(key=lambda p:len(p))
 
-    permuted_partitions = []
+    # Permute all partitions to get all compositions.
+    compositions = []
     for p in partitions:
-        permuted_partitions.append(list(itertools.permutations(p)))
+        compositions.append(list(itertools.permutations(p)))
 
-    consolidated_partitions = []
-    for p in permuted_partitions:
-        consolidated_partitions.append(list(dict.fromkeys(p)))
+    # Remove duplicate compositions (weird Python way).
+    consolidated_compositions = []
+    for c in compositions:
+        consolidated_compositions.append(list(dict.fromkeys(c)))
 
+    # Group partitions of the same length into lists.
     grouped_partitions = [[] for i in range(k)]
-
     i = 0
-    for p in consolidated_partitions:
-        for q in p:
+    for c in consolidated_compositions:
+        for q in c:
             if len(q) != i+1:
                 i += 1
             grouped_partitions[i].append(list(q))
@@ -263,79 +241,80 @@ def r_tuples(k):
     return grouped_partitions
 
 
-# Returns C_n(s) for a given n and s
 def C(s, n, weights):
+    '''
+    Returns C_n(s) for a given n and s.
+    '''
     if n % 2 == 1 or n <= 0:
         return 0
     
     sum = mp.mpf(0)
     for w in weights:
-        # print(w)
         frac = mp.fdiv(mp.power(w, mp.fmul(2 ,s)), (1 - mp.power(w, 2)))
         sum = mp.fadd(sum, frac)
-    # print (2 / n)
     return mp.fmul(sum, mp.fdiv(2, n))
 
 
-# Returns b_2k(s) for a given k and s
-def b(s, k, partitions, C_n):
+def b(k, compositions, C_n):
+    '''
+    Returns b_2k(s) for a given k and precomputed partitions and C_n(s).
+    '''
     if k <= 0:
         return 0
     
     sum = mp.mpf(0)
     for j in range(k):
-        for p in partitions[k][j]:
-            # print(p)
+        for p in compositions[k][j]:
             r = len(p)
             factor = mp.fdiv(mp.power(-1, r), mp.factorial(r))
             prod = mp.mpf(1)
             for n in p:
-                # print(n)
                 prod = mp.fmul(prod, C_n[2*n])
             sum = mp.fadd(sum, (mp.fmul(factor, prod)))
-    # print(sum)
     return sum
 
 
+# Set the decimal precision for the mpmath package.
+mp.dps = 52
 
+# The subset of the natural numbers which we restict our coefficients to.
+family = [mp.mpf(2), mp.mpf(5)]
 
-# print(p(4, [1, 5, 2, 2]))
-# print(q(4, [1, 5, 2, 2]))
-
-# print(my_cf_eval([1, 1] * 20))
-
-
-alph = [mp.mpf(2), mp.mpf(5)]
-# N = 10
+# Accuracy parameter - this controls how accurate our continued fraction
+# calculations are. Higher is more accurate but slower. A value of 200 gives
+# no error (compared to JP2001) when family = [1, 2], so we take this to be a
+# safe value for all sets.
 acc = 200
-# s_N = 0.5306685908375786932401360892819672638338139332442
 
 
-print("family=", alph)
+print("family=", family)
 print("acc =", acc)
 
-# for N in range(16,16):
+for N in range(2, 17, 2):
+    time_start = time.time()
 
-N = 10
+    lookup = get_full_lookup(family, N, acc)
 
-time_start = time.time()
+    compositions = [find_all_compositions(k) for k in range(0, (N // 2) + 1)]
 
-lookup = get_full_lookup(alph, N, acc)
+    weights = [[l[s].weight for s in l] for l in lookup]
 
-partitions = [r_tuples(k) for k in range(0, (N // 2) + 1)]
+    def est(s, N=N, compositions=compositions, weights=weights):
+        '''
+        Returns an estimate of the function D(s, 1). In particular, it is the
+        degree-N truncation of the sum.
+        '''
+        C_n =  [C(s, n, weights[n]) for n in range(N + 1)]
+        sum = mp.mpf(1)
+        for k in range(1, (N // 2) + 1):
+            sum = mp.fadd(sum, b(k, compositions, C_n))
+        return sum
 
-weights = [[l[s].weight for s in l] for l in lookup]
+    print("N =", N)
 
-def est(s, N=N, partitions=partitions, weights=weights):
-    C_n =  [C(s, n, weights[n]) for n in range(N + 1)]
-    sum = mp.mpf(1)
-    for k in range(1, (N // 2) + 1):
-        sum = mp.fadd(sum, b(s, k, partitions, C_n))
-    return sum
+    # mpmath function to find the root of est in the interval (0, 1).
+    mp.nprint(mp.findroot(est, (0, 1), solver="ridder", verbose=False), mp.dps)
 
-print("N =", N)
-mp.nprint(mp.findroot(est, (0, 1), solver="ridder", verbose=True), mp.dps)
+    time_end = time.time()
 
-time_end = time.time()
-
-print("time:", time_end - time_start, "seconds")
+    print("time:", time_end - time_start, "seconds")
